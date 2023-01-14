46c15edf17c4821cdb0c1117bb2f71f86e68b2f5
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2023-01-15 00:13:48 +0700

    Changing package for correct work

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,13 +1,21 @@
 export default function pug() {
   const {src, dest} = app.gulp
   const {build, source} = app.path
-  const {browserSync, gulpIf, replace, pug, webpHtmlNosvg} = app.plugins
+  const {browserSync, gulpIf, replace, pug, htmlImgWrapper} = app.plugins
 
   return src(source.pug)
     .pipe(app.errorHandler('PUG'))
     .pipe(pug({pretty: app.isDev}))
     .pipe(replace(/@img\//g, 'images/'))
-    .pipe(gulpIf(app.isBuild, webpHtmlNosvg()))
+    .pipe(
+      gulpIf(
+        app.isBuild,
+        htmlImgWrapper({
+          logger: true, // false for not showing message with amount of wrapped img tags for each file
+          extensions: ['.jpg', '.png', '.jpeg'] // write your own extensions pack (case insensitive)
+        })
+      )
+    )
     .pipe(dest(build.html))
     .pipe(browserSync.stream())
 }
