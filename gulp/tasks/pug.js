ea3298efdefa713481ea8a1e1d6b4872c592ac9e
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2023-01-15 21:31:41 +0700

    Adding gulp-data to the project

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,10 +1,27 @@
+import fs from 'fs'
+import url from 'url'
+import path from 'path'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default function pug() {
   const {src, dest} = app.gulp
   const {build, source} = app.path
-  const {browserSync, gulpIf, replace, pug, htmlImgWrapper} = app.plugins
+  const {browserSync, gulpIf, replace, pug, htmlImgWrapper, data} = app.plugins
 
   return src(source.pug)
     .pipe(app.errorHandler('PUG'))
+    .pipe(
+      data(() => {
+        return JSON.parse(
+          fs.readFileSync(
+            path.resolve(__dirname, '..', '..', 'source', 'data.json'),
+            'utf-8'
+          )
+        )
+      })
+    )
     .pipe(pug({pretty: app.isDev}))
     .pipe(replace(/@img\//g, 'images/'))
     .pipe(
